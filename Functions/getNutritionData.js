946e5a92d80550601c901e9176d3c6a541323909
visibly0ptimistic <victorejiasi@yahoo.com>
2023-08-03 10:56:35 -0500

    Data

--- Functions/getNutritionData.js
+++ Functions/getNutritionData.js
@@ -3,20 +3,33 @@
 const axios = require('axios');
 
 exports.handler = async function(event, context) {
+    // Get the food item from the query string
     const foodItem = event.queryStringParameters.food;
 
-    const apiUrl = `https://api.edamam.com/api/nutrition-data?app_id=${process.env.EDAMAM_APP_ID}&app_key=${process.env.EDAMAM_APP_KEY}&ingr=${foodItem}`;
+    // Define the API endpoint and parameters
+    const apiEndpoint = 'https://api.edamam.com/api/recipes/v2';
+    const apiParams = {
+        type: 'public',
+        q: foodItem,
+        app_id: process.env.EDAMAM_APP_ID,
+        app_key: process.env.EDAMAM_APP_KEY,
+    };
 
     try {
-        const response = await axios.get(apiUrl);
+        // Send a GET request to the API
+        const response = await axios.get(apiEndpoint, { params: apiParams });
+
+        // Return the data from the response
         return {
             statusCode: 200,
-            body: JSON.stringify(response.data)
+            body: JSON.stringify(response.data.hits),
         };
     } catch (error) {
+        // If there was an error, return a 500 error status code and the error message
         return {
             statusCode: 500,
-            body: JSON.stringify({ error: 'Failed to fetch nutritional data' })
+            body: JSON.stringify({ error: error.message }),
         };
     }
 };
+
